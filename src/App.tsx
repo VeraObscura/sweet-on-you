5165d6cd1fa960740a597bbbacf441c1c769b5ab
Vera Obscura <veraobscura@gmail.com>
2022-09-22 20:21:47 -0400

    initial commit of bakery audition scene

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import { Routes, Route } from "react-router-dom";
 
 import ActionPanel from "shared/actionPanel";
 //import Phonograph from "shared/phonograph";
-import StartMenu from "components/menus/startMenu";
-import PauseMenu from "components/menus/pauseMenu";
-import About from "components/menus/about";
-import CherryHeartCake from "scenes/3_cherryHeartCake";
-import CrumbCoatScene from "scenes/3_cherryHeartCake/components/crumbCoatScene";
-import CakeScene from "scenes/3_cherryHeartCake/components/cakeScene";
+import StartMenu from "shared/menus/startMenu";
+import PauseMenu from "shared/menus/pauseMenu";
+import About from "shared/menus/about";
+import Intro from "scenes/0_intro";
+import CherryAlmondCake from "scenes/3_cherryAlmondCake/cherryAlmondCake";
 import routes from "./routes";
 
 import "./app.scss";
 import { useState } from "react";
-import OptionsMenu from "components/menus/optionsMenu";
+import OptionsMenu from "shared/menus/optionsMenu";
+import BakeryAudition from "scenes/1_bakeryAudition";
+import FirstBigOrder from "scenes/2_firstBigOrder";
+import FallingInLove from "scenes/4_fallingInLove";
 
 function App() {
   const [displayPauseMenu, setDisplayPauseMenu] = useState(false);
@@ -58,11 +60,14 @@ function App() {
           }
         />
         <Route path={routes.ABOUT} element={<About />} />
-        <Route path={routes.CHERRY_HEART_CAKE}>
-          <Route path="" element={<CherryHeartCake />} />
-          <Route path={routes.CRUMB_COAT} element={<CrumbCoatScene />} />
-          <Route path={routes.CAKE_SCENE} element={<CakeScene />} />
-        </Route>
+        <Route path={routes.INTRO} element={<Intro />} />
+        <Route path={routes.BAKERY_AUDITION} element={<BakeryAudition />} />
+        <Route path={routes.FIRST_BIG_ORDER} element={<FirstBigOrder />} />
+        <Route
+          path={routes.CHERRY_ALMOND_CAKE}
+          element={<CherryAlmondCake />}
+        />
+        <Route path={routes.FALLING_IN_LOVE} element={<FallingInLove />} />
       </Routes>
     </div>
   );
---
+++ src/scenes/1_bakeryAudition/text/index.ts
@@ -0,0 +1,90 @@
+import { SlideType } from "types/slideTypes";
+
+export const slideText: SlideType[] = [
+  {
+    narration: {
+      en: "THE BIG AUDITION",
+    },
+  },
+  {
+    narration: {
+      en: "Mariela gently places her creation in a cakebox, ties it with coloured ribbon...",
+    },
+  },
+  {
+    narration: {
+      en: "...and makes her way to the bakery.",
+    },
+  },
+  {
+    stepName: "bakeryStoreFront",
+  },
+  {
+    narration: {
+      en: "Her heart beats nervously as she steps inside...",
+    },
+  },
+  {
+    stepName: "bakeryInterior",
+  },
+  {
+    title: {
+      en: "Bakery Owner",
+    },
+    dialogue: {
+      en: "Ah, we spoke on the phone. I'm glad you could make it.",
+    },
+  },
+  {
+    title: {
+      en: "Bakery Owner",
+    },
+    dialogue: {
+      en: "So this is the cake? How beautiful! But how does it taste? Let's see…",
+    },
+  },
+  {
+    stepName: "cakeEaten",
+  },
+  {
+    title: {
+      en: "Bakery Owner",
+    },
+    dialogue: {
+      en: "Oh my, how scrumptious!",
+    },
+  },
+  {
+    title: {
+      en: "Bakery Owner",
+    },
+    dialogue: {
+      en: "These will be a hit with customers for sure!",
+    },
+  },
+  {
+    dialogue: {
+      en: "I'd like to order five more for next week. How does that sound?",
+    },
+    choices: [{ en: "Yes!" }],
+  },
+];
+
+export const pathA: SlideType[] = [
+  {
+    title: {
+      en: "Bakery Owner",
+    },
+    dialogue: {
+      en: "I'll pay you for this one, as well as the deposit for next week's order.",
+    },
+  },
+  {
+    title: {
+      en: "Bakery Owner",
+    },
+    dialogue: {
+      en: "Looking forward to doing business with you!",
+    },
+  },
+];

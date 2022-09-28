17668721d8d9d06318c642c0157a833aff1a6ac7
Vera Obscura <veraobscura@gmail.com>
2022-09-28 08:38:20 -0400

    groundwork for 'Show Must Go On' scene

---
+++ src/scenes/6_showMustGoOn/text/index.ts
@@ -0,0 +1,264 @@
+import { SlideType } from "types/slideTypes";
+
+export const slidesA: SlideType[] = [
+  {
+    chapter: {
+      en: "On with the show",
+    },
+  },
+  {
+    context: {
+      en: "Two days before the big tasting...",
+    },
+  },
+  {
+    stepName: "bakeryStoreFront",
+  },
+  {
+    narration: {
+      en: "The sound of the knock at the door startles Mariela,",
+    },
+  },
+  {
+    narration: {
+      en: "...and sound rarely heard since she never invited anyone over.",
+    },
+  },
+  {
+    stepName: "bakeryStoreFront",
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "Um, hello! I'm Julia...",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "I'm the woman who sent you the order for the wedding cake.",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: " I'm sorry, wish I could have gotten to you sooner...",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "...but planning this wedding has been an absolute whirlwind, and I'm not often in this part of town.",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "I know this is last minute, but I'll have to cancel our tasting.",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "My fiance and I have talked it over, we've decided to just go with a cake with our favourite flavor...",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "...traditions and in-laws be damned.",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "It's our special day, you know?",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "More importantly, I also wanted to apologize for what happened at the bakery.",
+    },
+  },
+  {
+    stepName: "bakeryStoreFront",
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "I heard from the bakery owner that you waited all day in the cafe for me? ",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "I just feel terrible.",
+    },
+  },
+  {
+    stepName: "bakeryStoreFront",
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "Please let me explain - ",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "- that day happened to be the day that my fiance proposed to me.",
+    },
+  },
+  {
+    stepName: "bakeryStoreFront",
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "I'd like to make it up to you though -",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "I own two restaurants, and am opening another next spring...",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "...I'd love for you to be our head pastry chef.",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "I know that's a lot, but maybe think about it?",
+    },
+  },
+  {
+    stepName: "bakeryStoreFront",
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "I did mean what I said in those letters. ",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: " I think your work is amazing, and I wouldn't trust anyone else with this job.",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "If you still want to, I'd still love to grab a coffee sometime.",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "Though maybe after the wedding?",
+    },
+  },
+  {
+    stepName: "bakeryStoreFront",
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "Anyway, I'll make sure Robert pays you extra for the trouble you've gone through already. ",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "He'll leave it for you in an envelope with the bakery owner...",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "...like he always does.",
+    },
+  },
+  {
+    stepName: "bakeryStoreFront",
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "Oh! Silly me, I didn't even mention the flavor, but I bet you could guess...",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "...a white chocolate cherry cake with almond frosting.",
+    },
+  },
+  {
+    title: {
+      en: "Julia",
+    },
+    dialogue: {
+      en: "I have to leave now for a fitting, take care now!",
+    },
+  },
+];

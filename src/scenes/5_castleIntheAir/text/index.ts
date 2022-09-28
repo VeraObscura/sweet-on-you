59694051cb8dcc73a19a21bbf5e7a385e6eb02ae
Vera Obscura <veraobscura@gmail.com>
2022-09-28 08:37:39 -0400

    groundwork for 'Castle In the Air' scene

---
+++ src/scenes/5_castleIntheAir/text/index.ts
@@ -0,0 +1,163 @@
+import { SlideType } from "types/slideTypes";
+
+export const slidesA: SlideType[] = [
+  {
+    chapter: {
+      en: "Castle in the air",
+    },
+  },
+  {
+    context: {
+      en: "One week later...",
+    },
+  },
+  {
+    narration: {
+      en: "...the day had finally come. No longer would the lonely baker Mariela pine away for her J.W.",
+    },
+  },
+  {
+    narration: {
+      en: "She went to the bakery early in the morning, determined to meet her mysterious patron.",
+    },
+  },
+  {
+    narration: {
+      en: "Dressed as well as she could manage, she chose a seat by the window and waited. ",
+    },
+  },
+  {
+    narration: {
+      en: "And waited.",
+    },
+  },
+  {
+    stepName: "bakeryStoreFront",
+  },
+  {
+    narration: {
+      en: "And waited...",
+    },
+  },
+  {
+    stepName: "bakeryStoreFront",
+  },
+  {
+    narration: {
+      en: "J.W. never came that day.",
+    },
+  },
+  {
+    stepName: "bakeryStoreFront",
+  },
+  {
+    narration: {
+      en: "When she dropped off her delivery the following week, the bakery owner had no envelope from J.W.",
+    },
+  },
+  {
+    narration: {
+      en: "And the week after that, no letter once more.",
+    },
+  },
+  {
+    stepName: "bakeryStoreFront",
+  },
+  {
+    narration: {
+      en: "The months wore on, and word of Mariela's beautiful cakes spread far and wide.",
+    },
+  },
+  {
+    narration: {
+      en: "Now, she was taking custom orders.",
+    },
+  },
+  {
+    narration: {
+      en: "A Lemon Chiffon cake with Apricot Icing for a christening...",
+    },
+  },
+  {
+    narration: {
+      en: "...a Rose and Pistachio Cream Cake for a birthday...",
+    },
+  },
+  {
+    narration: {
+      en: "...even a decadent Chocolate Whiskey and Salted Caramel buttercream for the governor's ball.",
+    },
+  },
+  {
+    narration: {
+      en: "And yet, still no word from the mysterious J.W.",
+    },
+  },
+  {
+    narration: {
+      en: "She knew she should just let it go...",
+    },
+  },
+  {
+    narration: {
+      en: "But she couldn't help it - the torch she carried for him burned as brightly as ever.",
+    },
+  },
+  {
+    narration: {
+      en: "And then one day...",
+    },
+  },
+  {
+    title: {
+      en: "Bakery Owner",
+    },
+    dialogue: {
+      en: "Mariela! Great news!",
+    },
+  },
+  {
+    narration: {
+      en: "The baker was almost beside himself with excitment...",
+    },
+  },
+  {
+    narration: {
+      en: "...as he clutched a familiar envelope in his hand.",
+    },
+  },
+  {
+    narration: {
+      en: "Mariela's heart leapt in her chest -",
+    },
+  },
+  {
+    title: {
+      en: "Bakery Owner",
+    },
+    dialogue: {
+      en: "You've got a custom order - your first wedding cake!",
+    },
+  },
+  {
+    narration: {
+      en: "- and then sank again.",
+    },
+  },
+  {
+    title: {
+      en: "Bakery Owner",
+    },
+    dialogue: {
+      en: "It's for a woman who's been impressed by your work. She seemed like a big shot. She passed this on.",
+    },
+  },
+  {
+    stepName: "bakeryStoreFront",
+  },
+  {
+    narration: {
+      en: "Deflated, Mariela returns home with her work cut out for her, and sighs to herself...",
+    },
+  },
+];

3a8823aa09be8a5229dfeeea3387f95b75bedcc8
Vera Obscura <veraobscura@gmail.com>
2022-10-10 12:29:23 -0400

    added global variables

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,15 @@ export default defineConfig({
         : undefined,
     }),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "@/styles/global";
+        `,
+      },
+    },
+  },
   server: process.env.VSCODE_DEBUG
     ? (() => {
         const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL);

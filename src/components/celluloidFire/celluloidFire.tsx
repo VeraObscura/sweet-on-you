a3ffdf159e3340bec346952c6f3ada53cbcfc14c
Vera Obscura <veraobscura@gmail.com>
2022-11-27 21:09:14 -0500

    updated celluloid fire component to use gif

--- src/components/celluloidFire/celluloidFire.tsx
+++ src/components/celluloidFire/celluloidFire.tsx
@@ -1,54 +1,12 @@
 import { useState, useEffect } from "react";
 import styles from "./celluloidFire.module.scss";
 
-interface FlameProps {
-  top: string;
-  left: string;
-  version: "small" | "large";
-}
-
-const Flame = ({ top, left, version }: FlameProps) => {
-  const numrows = 50;
-  const particles = [];
-
-  for (let i = 0; i < numrows; i++) {
-    particles.push(
-      <div
-        key={i}
-        className={`${styles.particle} ${
-          version === "small" ? styles.particleSmall : styles.particleLarge
-        }`}
-      />
-    );
-  }
-
-  return (
-    <div className={styles.fire} style={{ top: top, left: left }}>
-      {particles.map((part) => {
-        return part;
-      })}
-      ;
-    </div>
-  );
-};
+import filmBurn from "@/assets/images/filmBurn.gif";
 
 const CelluloidFire = () => {
-  const [showFlame1, setShowFlame1] = useState(false);
-  const [showFlame2, setShowFlame2] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setShowFlame1(true);
-    }, 3000);
-    setTimeout(() => {
-      setShowFlame2(true);
-    }, 5000);
-  }, []);
-
   return (
     <div className={styles.celluloidFire}>
-      {showFlame1 && <Flame top={"0%"} left={"10%"} version="small" />}
-      {showFlame2 && <Flame top={"-20%"} left={"60%"} version="large" />}
+      <img src={filmBurn} alt="film burn" />
     </div>
   );
 };
